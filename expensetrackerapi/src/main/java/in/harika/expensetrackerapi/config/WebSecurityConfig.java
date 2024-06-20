287d7ca97c4501d38e6b449d27aecea395ec529a
harika234 <harikacdm@gmail.com>
2024-06-20 16:59:28 +0530

    swagger doc

--- expensetrackerapi/src/main/java/in/harika/expensetrackerapi/config/WebSecurityConfig.java
+++ expensetrackerapi/src/main/java/in/harika/expensetrackerapi/config/WebSecurityConfig.java
@@ -11,62 +11,55 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
     @Bean
     public JwtRequestFilter authenticationJwtTokenFilter() {
         return new JwtRequestFilter();
     }
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login","/register").permitAll()
+                .antMatchers("/login", "/register").permitAll()
+                .antMatchers(
+                        "/v2/api-docs",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui/index.html").permitAll()  // Permit access to Swagger endpoints
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
-                http.httpBasic();
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.httpBasic();
     }
 
-    protected  void configure(AuthenticationManagerBuilder auth) throws Exception {
-//       auth.inMemoryAuthentication()
-//               .withUser("Harika").password("12345").authorities("admin")
-//               .and()
-//               .withUser("harry").password("12345").authorities("user")
-//               .and()
-//               .passwordEncoder(NoOpPasswordEncoder.getInstance());
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//        UserDetails user1 = User.withUsername("harika").password("12345").authorities("admin").build();
-//        UserDetails user2 = User.withUsername("harry").password("12345").authorities("user").build();
-//        userDetailsManager.createUser(user1);
-//        userDetailsManager.createUser(user2);
-//
-//        auth.userDetailsService(userDetailsManager);
-
-        auth.userDetailsService(userDetailsService);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception{
+    public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 }
